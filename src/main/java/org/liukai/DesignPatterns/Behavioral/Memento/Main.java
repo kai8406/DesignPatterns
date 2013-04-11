83d27262a55ab4ae9a736d788f83b90e1806fc79
liukai <kai8406@gmail.com>
2013-04-11 21:33:16 +0800

    Memento 模式
    备忘录模式

---
+++ src/main/java/org/liukai/DesignPatterns/Behavioral/Memento/Main.java
@@ -0,0 +1,29 @@
+package org.liukai.DesignPatterns.Behavioral.Memento;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Person per = new Person("lifengxing", "男", 24);
+
+		// Memento memento = per.createMemento();
+		Caretaker caretaker = new Caretaker();
+		caretaker.setMemento(per.createMemento());
+
+		per.display();
+
+		per.setName("beifeng");
+		per.setSex("女");
+		per.setAge(1);
+
+		per.display();
+
+		per.setMemento(caretaker.getMemento());
+		per.display();
+
+	}
+
+}
